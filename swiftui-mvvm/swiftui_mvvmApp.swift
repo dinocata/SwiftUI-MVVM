00d742b928736d0f3978f862c26a10ee29e14038
Dino Catalinac <dino.catalinac@iacapps.com>
2024-01-11 10:05:03 +0100

    Initial Commit

---
+++ swiftui-mvvm/swiftui_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvmApp.swift
+//  swiftui-mvvm
+//
+//  Created by Dino Catalinac on 11.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
